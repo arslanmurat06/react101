0de796124c703d0231961b7bd4c5a2dbab7c6f7b
arslanmurat06 <mrtarsln06@gmail.com>
2021-08-17 11:22:08 +0300

    alert added when employee created

--- employee-erp/src/components/EmployeeList.js
+++ employee-erp/src/components/EmployeeList.js
@@ -1,17 +1,31 @@
 import Employee from "./Employee";
 import { EmployeeContext } from "../contexts/EmployeeContext";
 import { useContext, useState, useEffect } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Alert } from "react-bootstrap";
 import AddForm from "./AddForm";
 
 const EmployeeList = () => {
   const { employees } = useContext(EmployeeContext);
   const [isShowModal, setShowModal] = useState(false);
+  const [isShowAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
     console.log("useeffecte geldi");
     return () => {
       setShowModal(false);
+    };
+  }, [employees]);
+
+  const handleShowAlert = () => {
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    return () => {
+      handleShowAlert();
     };
   }, [employees]);
 
@@ -36,6 +50,14 @@ const EmployeeList = () => {
           </div>
         </div>
       </div>
+      <Alert
+        variant="success"
+        show={isShowAlert}
+        onClose={() => isShowAlert(false)}
+        dismissible
+      >
+        <Alert.Heading>Successfull created</Alert.Heading>
+      </Alert>
       <table className="table table-striped table-hover">
         <thead>
           <tr>
